46a4fa898dbdc23e8ddc8b4c59716874761d18d4
Pete MC-Mods <mcmods.pete@gmail.com>
2026-06-14 23:35:49 +0200

    port version 1.4.0 to Forge for Minecraft 26.1

--- src/main/java/net/petemc/daycount/client/DayCountHud.java
+++ src/main/java/net/petemc/daycount/client/DayCountHud.java
@@ -2,49 +2,39 @@ package net.petemc.daycount.client;
 
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.petemc.daycount.DayCount;
 import net.petemc.daycount.config.MainConfig;
-import org.jetbrains.annotations.NotNull;
-import org.joml.Matrix3x2fStack;
 
-@OnlyIn(Dist.CLIENT)
-public class DayCountHud implements Gui.RenderFunction {
+public class DayCountHud {
     public static DayCountHud DAY_COUNT_HUD_INSTANCE;
-    private static String currentTextColor = "FFFFFFFF";
 
     public static void setCurrentTextColor(String value) {
         currentTextColor = value;
     }
-
     public String getCurrentTextColor() {
         return currentTextColor;
     }
+    private static String currentTextColor = "FFFFFFFF";
 
     public static void init() {
         DAY_COUNT_HUD_INSTANCE = new DayCountHud();
     }
 
-    @Override
-    public void render(@NotNull GuiGraphics guiGraphics, DeltaTracker pDeltaTracker) {
+    public void render(GuiGraphicsExtractor guiGraphics, DeltaTracker pDeltaTracker) {
         if (DayCount.dayCountEnabled) {
             Minecraft mc = Minecraft.getInstance();
-            if (mc.level != null) {
-                int currentDay = (int) (mc.level.getDayTime() / 24000L);
-                if (mc.gameMode != null) {
-                    if ((mc.gameMode.getPlayerMode().isSurvival() || mc.gameMode.getPlayerMode().isCreative()) &&
-                            (!mc.getDebugOverlay().showDebugScreen() || MainConfig.getDisplayDayCountWhileShowingF3Info())) {
-                        Matrix3x2fStack matrixStack = guiGraphics.pose();
-                        matrixStack.pushMatrix();
-                        matrixStack.translate(MainConfig.getLocationX(), MainConfig.getLocationY(), matrixStack);
-                        matrixStack.scale(MainConfig.getSizeX(), MainConfig.getSizeY(), matrixStack);
-                        guiGraphics.drawString(mc.font, MainConfig.getDayCounterString() + (currentDay + MainConfig.getDayOffset()), 1, 1, (int) Long.parseLong(currentTextColor, 16));
-                        matrixStack.popMatrix();
-                    }
-                }
+            assert mc.level != null;
+            int currentDay = (int) (mc.level.getOverworldClockTime() / 24000L);
+            assert mc.gameMode != null;
+            if ((mc.gameMode.getPlayerMode().isSurvival() || mc.gameMode.getPlayerMode().isCreative()) &&
+                    (!mc.getDebugOverlay().showDebugScreen() || MainConfig.getDisplayDayCountWhileShowingF3Info())) {
+                var pose = guiGraphics.pose();
+                pose.pushMatrix();
+                pose.translate(MainConfig.getLocationX(), MainConfig.getLocationY());
+                pose.scale(MainConfig.getSizeX(), MainConfig.getSizeY());
+                guiGraphics.text(mc.font, MainConfig.getDayCounterString() + (currentDay + MainConfig.getDayOffset()), 1, 1, (int) Long.parseLong(currentTextColor, 16));
+                pose.popMatrix();
             }
         }
     }
